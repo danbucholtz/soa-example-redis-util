8f8ae58147839297facf94f4aa55264be3fb817a
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-30 15:37:57 -0600

    updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var redis = require("redis");
 var config = require("soa-example-service-config").config();
+var Q = require("q");
 
 var redisClient = redis.createClient();
 
@@ -11,6 +12,41 @@ var getRedisClient = function(){
 	return redisClient;
 };
 
+var put = function (key, object){
+	if ( object ){
+		var json = JSON.stringify(object);
+		redisClient.set(key, json);
+	}
+};
+
+var purge = function(key){
+	// todo
+};
+
+var purgeAll = function(){
+	// todo
+}
+
+var get = function (key){
+	var deferred = Q.defer();
+
+	redisClient.get(key, function(err, json){
+		var object = null;
+		try{
+			object = JSON.parse(json);
+		}
+		catch(ex){
+		}
+		deferred.resolve(object);
+	});
+
+	return deferred.promise;
+};
+
 module.exports = {
-	getRedisClient : getRedisClient
+	getRedisClient : getRedisClient,
+	put: put,
+	get: get,
+	purge: purge,
+	purgeAll: purgeAll
 }
